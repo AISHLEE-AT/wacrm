58f91192b102176377a6784cb80c405ea611a52d
aishleeraadee <aishleeraadee@gmail.com>
2026-08-28 11:46:36 +0530

    Hotfix: Fix ImmersiveVideoPlayer string template escaping in PowerShell

--- apps/web/src/components/teacho/ImmersiveVideoPlayer.tsx
+++ apps/web/src/components/teacho/ImmersiveVideoPlayer.tsx
@@ -4,7 +4,7 @@ export const ImmersiveVideoPlayer = ({ videoId, title }: { videoId: string, titl
   <div className="w-full aspect-video rounded-xl overflow-hidden bg-black">
     <iframe
       className="w-full h-full"
-      src={https://www.youtube.com/embed/}
+      src={`https://www.youtube.com/embed/${videoId}`}
       title={title || "Video Player"}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
